8ed41061e8d66048c63d38ae89c4a718fcc554c6
Anon <anon@example.com>
2025-02-15 05:34:49 +0000

    deploy

--- writer.js
+++ writer.js
@@ -19,6 +19,8 @@ writer.write = async (subject, background) => {
       .replaceAll(`“`,`"`)
       .replaceAll(`”`,`"`)
       .replaceAll(`…`,`...`)
+      .replaceAll('```html','')
+      .replaceAll('```','')
       .replace(/[^\x09\x0A\x0D\x20-\x7E\xA3\u20AC]/g, '');
     const regex = /(title|slug|description|image|category):\s*"([^"]+)"/g;
     const article = {};
